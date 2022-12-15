84778347e547bb971155424040d76fc4771c2862
Risad212 <hafez.risad@gmail.com>
2022-12-15 16:13:31 +0600

    done

--- Components/Header/Header.jsx
+++ Components/Header/Header.jsx
@@ -4,35 +4,33 @@ import header from './Header.module.css';
 const Header = () => {
     return (
         <div className={header.header}>
-            <div className="container">
-                <nav className="navbar navbar-expand-lg navbar-light">
-                    <div className="container-fluid">
-                        <a className={header.navbarBrand} href="#">NEXT PORTFOLIO</a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                            <ul className="navbar-nav ms-auto">
-                                <li className={header.navItem}>
-                                    <a className="nav-link"href="#">Home</a>
-                                </li>
-                                <li className={header.navItem}>
-                                    <a className="nav-link" href="#">About Me</a>
-                                </li>
-                                <li className={header.navItem}>
-                                    <a className="nav-link" href="#">Services</a>
-                                </li>
-                                <li className={header.navItem}>
-                                    <a className="nav-link" href="#">Portfolio</a>
-                                </li>
-                                <li className={`${header.navItem} ${header.navButton}`}>
-                                    <a className="nav-link" href="#">Contact Me</a>
-                                </li>
-                            </ul>
-                        </div>
+            <nav className="navbar navbar-expand-lg navbar-light">
+                <div className="container">
+                    <a className={header.navbarBrand} href="#">NEXT PORTFOLIO</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul className="navbar-nav ms-auto">
+                            <li className={header.navItem}>
+                                <a className="nav-link" href="#">Home</a>
+                            </li>
+                            <li className={header.navItem}>
+                                <a className="nav-link" href="#">About Me</a>
+                            </li>
+                            <li className={header.navItem}>
+                                <a className="nav-link" href="#">Services</a>
+                            </li>
+                            <li className={header.navItem}>
+                                <a className="nav-link" href="#">Portfolio</a>
+                            </li>
+                            <li className={`${header.navItem} ${header.navButton}`}>
+                                <a className="nav-link" href="#">Contact Me</a>
+                            </li>
+                        </ul>
                     </div>
-                </nav>
-            </div>
+                </div>
+            </nav>
         </div>
     );
 };
