70738ccaeb3f38bfce281e1cc60d5846f46e4291
Risad212 <hafez.risad@gmail.com>
2022-12-17 08:09:37 +0600

    done

--- Components/Footer/Footer.jsx
+++ Components/Footer/Footer.jsx
@@ -1,10 +1,5 @@
 import Image from 'next/image';
 import React from 'react';
-/*-- import social icons --*/
-import fb from '../../Media/fb.svg';
-import twi from '../../Media/twi.svg';
-import inst from '../../Media/inst.svg';
-import linkin from '../../Media/linkin.svg'
 import footer from './Footer.module.css';
 
 const Footer = () => {
@@ -12,41 +7,21 @@ const Footer = () => {
         <div className={footer.footer}>
             <div className="container">
                 <div className="row">
-                    <div className="col-lg-4 col-md-6">
-                    <div className={footer.footer1}>
-                        <h3>NEXT PORTFOLIO</h3>
-                        <p>We have tested a number of registry fix and clean utilities and present our top 3 list on our site for your convenience.Copyright ©2022 All rights reserved | This template is made with  by Colorlib</p>
-                    </div>
-                    </div>
-                    <div className="col-lg-2 col-md-6">
-                    <div className={footer.footer2}>
-                        <h3>Features</h3>
-                        <ul>
+                    <div className="col-12 text-center">
+                        <h3><a href="#">NEXT PORTFOLIO</a></h3>
+                        <ul className={footer.footerLink}>
                             <li><a href="#">Home</a></li>
-                            <li><a href="#">About Me</a></li>
+                            <li><a href="#">About</a></li>
                             <li><a href="#">Services</a></li>
                             <li><a href="#">Portfolio</a></li>
                             <li><a href="#">Contact Me</a></li>
                         </ul>
-                    </div>
-                    </div>
-                    <div className="col-lg-2 col-md-6 ps-3">
-                    <div className={footer.footer3}>
-                        <h3>Folow Us</h3>
-                        <ul>
+                        <ul className={footer.socialIcon}>
                             <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
                             <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
                             <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
                             <li><a href="#"><i class="fa-brands fa-linkedin-in"></i></a></li>
                         </ul>
-                    </div>
-                    </div>
-                    <div className="col-lg-4 col-md-6">
-                    <div className={footer.footer4}>
-                        <h3>Subscribe Newsletter</h3>
-                         <input type="text" className={footer.input}/>
-                         <input type="submit" value="Submit" className={footer.submit}/>
-                    </div>
                     </div>
                 </div>
             </div>
