5ca4e6c24bd7822901ca08e72a2a1597c5bd10be
Risad212 <hafez.risad@gmail.com>
2023-01-06 10:47:55 +0600

    getting problem

---
+++ Components/DummyData/DummyData.js
@@ -0,0 +1,39 @@
+/*-- import images --*/
+import wp from '../../Media/wp.jpg';
+import react from '../../Media/react.jpg';
+import edu from '../../Media/edu.jpg';
+import tra from '../../Media/tra.jpg';
+import edi from '../../Media/edi.jpg';
+
+export const portfolioData = [
+    {
+        title: 'Dmarketing24',
+        link: 'https://www.dmarketing24.com/',
+        img: wp,
+        category: 'wordpress',
+    },
+    {
+        title: 'Sunlite Mortgage',
+        link: 'https://www.sunlitemortgage.ca/',
+        img: react,
+        category: 'react',
+    },
+    {
+        title: 'Edification',
+        link: 'https://edification.com.bd/english/',
+        img: edi,
+        category: 'webdesign'
+    },
+    {
+        title: 'Travel Agency',
+        link: '#',
+        img: tra,
+        category: 'webdesign'
+    },
+    {
+        title: 'Eduhum',
+        link: '#',
+        img: edu,
+        category: 'webdesign'
+    }
+]
--- Layouts/portfolio/Portfolio.jsx
+++ Layouts/portfolio/Portfolio.jsx
@@ -1,42 +1,18 @@
-import React from 'react';
+import React, {useState } from 'react';
 import portfolio from './portfolio.module.css';
 import Image from 'next/image';
-/*-- import images --*/
-import wp from '../../Media/wp.jpg';
-import react from '../../Media/react.jpg';
-import edu from '../../Media/edu.jpg';
-import tra from '../../Media/tra.jpg';
-import edi from '../../Media/edi.jpg';
+import { portfolioData } from '../../Components/DummyData/DummyData';
 
 
 const Portfolio = () => {
-    const portObj = [
-        {
-            title: 'Dmarketing24',
-            link: 'https://www.dmarketing24.com/',
-            img: wp,
-        },
-        {
-            title: 'Sunlite Mortgage',
-            link: 'https://www.sunlitemortgage.ca/',
-            img: react,
-        },
-        {
-            title: 'Edification',
-            link: 'https://edification.com.bd/english/',
-            img: edi,
-        },
-        {
-            title: 'Travel Agency',
-            link: '#',
-            img: tra,
-        },
-        {
-            title: 'Eduhum',
-            link: '#',
-            img: edu,
-        }
-    ]
+    const [demo, setDemo] = useState(portfolioData);
+    const buttonClick = (e) =>{
+      const btnId = e.target.id;
+      const updateDemo = demo.filter((elem) =>{
+            return btnId === elem.category
+      })
+       setDemo(updateDemo)
+     }
 
     return (
         <div className={portfolio.portfolio}>
@@ -46,15 +22,14 @@ const Portfolio = () => {
                     <h1>My Latest Projects</h1>
                 </div>
                 <ul className={portfolio.buttonList}>
-                    <li id="0"><button onClick={(e) => onIdSubmit(e.target.parentNode.id)}>all</button></li>
-                    <li id="1"><button>Web Design</button></li>
-                    <li id="2"><button>React</button></li>
-                    <li id="3"><button>Wordpress</button></li>
+                    <li><button id="all" onClick={(e) => buttonClick(e)}>all</button></li>
+                    <li><button id="webdesign" onClick={(e) => buttonClick(e)}>Web Design</button></li>
+                    <li><button id="react"onClick={(e) => buttonClick(e)}>React</button></li>
+                    <li><button id="wordpress" onClick={(e) => buttonClick(e)}>Wordpress</button></li>
                 </ul>
                 <div className="row g-5">
                     {
-                        portObj ?
-                            portObj.map((elem) => {
+                          demo.map((elem) => {
                                 return(
                                     <>
                                         <div className="col-lg-4 col-md-6 col-sm-12">
@@ -69,8 +44,6 @@ const Portfolio = () => {
                                     </>
                                 )
                             })
-                            :
-                           ''
                     }
                 </div>
             </div>
